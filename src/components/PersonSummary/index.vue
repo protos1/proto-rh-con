4161fce51295ad22368c2fb35322c04c0037ecbc
Renan Zozimo <renanfz@gmail.com>
2019-06-13 16:21:10 -0300

    Brand

--- src/components/PersonSummary/index.vue
+++ src/components/PersonSummary/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="s1-U__full-width s1-U__align-children--center">
     <md-avatar
-      class="md-avatar-icon md-primary s1-U__mg--rt8"
+      class="md-avatar-icon md-accent s1-U__mg--rt8"
       :class="avatarSize || ''"
     >
       <img :src="person.Photo" alt="Avatar" v-if="person.Photo" />
